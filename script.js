607338a058bd6aaac40181dd177291e8fbe7123d
Anm0lGupta <167634091+Anm0lGupta@users.noreply.github.com>
2025-08-01 21:09:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const bodyElement = document.querySelector("body");
+bodyElement.addEventListener("mousemove", (event) => {
+    const xposition = event.offsetX;
+    const yposition = event.offsetY;
+    const spanElement = document.createElement("span");
+    spanElement.style.left = xposition + "px";
+    spanElement.style.top = yposition + "px";
+    const size = Math.random() * 100;
+    spanElement.style.width = size + "px";
+    spanElement.style.height = size + "px";
+    bodyElement.appendChild(spanElement);
+    setTimeout(() => {
+        spanElement.remove();
+    }, 3000);
+
+});
